6b404d94304d99b98ce749a9eed0fd6278d2909b
Yury Vovk <gungravekoga@gmail.com>
2018-12-21 20:10:47 +0300

    WIP

--- wx/wxValidator.swift
+++ wx/wxValidator.swift
@@ -6,9 +6,70 @@
 //  Copyright © 2018 Yury Vovk. All rights reserved.
 //
 
-open class wxValidator: wxObject {
+@_silgen_name("wxValidator_Create")
+fileprivate func wxValidator_Create() -> CVoidPtr
+internal func _wxc_wxValidator_Create() -> CVoidPtr {
+    return wxValidator_Create()
+}
+
+@_silgen_name("wxValidator_Delete")
+fileprivate func wxValidator_Delete(_ ptr: CVoidPtr) -> Void
+internal func _wxc_wxValidator_Delete(_ ptr: CVoidPtr) -> Void {
+    wxValidator_Delete(ptr)
+}
+
+@_silgen_name("wxValidator_GetWindow")
+fileprivate func wxValidator_GetWindow(_ ptr: CVoidPtr) -> CVoidPtr
+internal func _wxc_wxValidator_GetWindow(_ ptr: CVoidPtr) -> CVoidPtr {
+    return wxValidator_GetWindow(ptr)
+}
+
+@_silgen_name("wxValidator_SetWindow")
+fileprivate func wxValidator_SetWindow(_ ptr: CVoidPtr, _ window: CVoidPtr) -> Void
+internal func _wxc_wxValidator_SetWindow(_ ptr: CVoidPtr, _ window: CVoidPtr) -> Void {
+    wxValidator_SetWindow(ptr, window)
+}
+
+open class wxValidator: wxEvtHandler {
     
     public required init?(rawValue: CVoidPtr) {
         super.init(rawValue: rawValue)
+    }
+    
+    public required init() {
+        super.init(rawValue: _wxc_wxValidator_Create())!
+    }
+    
+    public override func delete() {
+        guard rawValue != nil else {
+            return
+        }
+        
+        _wxc_wxValidator_Delete(rawValue)
+        rawValue = nil
+    }
+    
+    internal var _window: wxWindow?
+    open var window: wxWindow? {
+        get {
+            return getWindow()
+        }
+        
+        set {
+            setWindow(newValue)
+        }
+    }
+    
+    open func getWindow<T: wxWindow>() -> T? {
+        guard _window != nil else {
+            return T(rawValue: _wxc_wxValidator_GetWindow(rawValue))
+        }
+        
+        return _window as? T
+    }
+    
+    open func setWindow(_ window: wxWindow?) {
+        _wxc_wxValidator_SetWindow(rawValue, window?.rawValue)
+        _window = window
     }
 }
